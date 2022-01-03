9d990d59a109f747e49ca1cc06cf39ba142167ae
alcor67 <timur67@list.ru>
2022-01-04 01:53:20 +0200

    test changes to file test_complex.go

--- test_complex.go
+++ test_complex.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	x, y := complex(1, 2), complex(3, 4)
+	x, y, z := complex(1, 2), complex(3, 4), complex(5, 6)
 	fmt.Println(x, y)             // (1+2i) (3+4i)
 	fmt.Println(real(x), imag(x)) // 1 2
 	fmt.Println(x * y)            // (-5+10i)
 	fmt.Println(1i)               // (0+1i)
 	fmt.Println(1i * 1i)          // (-1+0i)
+	fmt.Println(x * z)
+	fmt.Println(z * y)
+	fmt.Println(x * y * z)
 }
